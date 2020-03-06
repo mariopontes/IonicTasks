7e06d778a7fcbeebed5ff2af983bb720d266dd3c
Mario <extern.mario.pontes@t-systems.com.br>
2020-03-06 12:57:31 -0300

    envrioment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCbNbbmvon2hxUgo3yt27OVwlJHmvLhhwU",
+    authDomain: "ionic-firestore-tasks-14ebe.firebaseapp.com",
+    databaseURL: "https://ionic-firestore-tasks-14ebe.firebaseio.com",
+    projectId: "ionic-firestore-tasks-14ebe",
+    storageBucket: "ionic-firestore-tasks-14ebe.appspot.com",
+    messagingSenderId: "205212343150",
+    appId: "1:205212343150:web:42ab49ff7934b69f403f7f",
+    measurementId: "G-KR5KS35CDV"
+  }
 };
